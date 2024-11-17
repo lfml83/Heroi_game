620f55e4eaee937d3fc29e2d3c924b194fca9b38
Luiz <luizfernando_leite@hotmail.com>
2024-11-17 16:35:06 -0300

    Final projeto

--- jogo.py
+++ jogo.py
@@ -2,6 +2,8 @@
 # Heroi : controlado pelo usuario
 # Inimigo : adversario do usuario
 
+import random
+
 class Personagem:
     def __init__(self,nome, vida, nivel):
         self.__nome= nome
@@ -15,10 +17,22 @@ class Personagem:
         return self.__vida
     
     def get_nivel(self):
-        return self.__vida
+        return self.__nivel
     
     def exibir_detalhes(self):
         return f'Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}'
+
+    def receber_ataque(self, dano):
+         self.__vida -= dano
+
+         if self.__vida < 0:
+              self.__vida =0    
+
+    def atacar(self, alvo):
+         dano = random.randint(self.get_nivel()*2,self.get_nivel()*4)
+         alvo.receber_ataque(dano)
+         print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
+
 
 class Heroi(Personagem):
     def __init__(self, nome, vida, nivel, habilidade):
@@ -30,7 +44,13 @@ class Heroi(Personagem):
         
     def exibir_detalhes(self):
             return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
-        
+
+    def ataque_especial(self, alvo):
+         dano = random.randint(self.get_nivel()*5,self.get_nivel()*8)
+         alvo.receber_ataque(dano)
+         print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano")
+
+
 class Inimigo(Personagem):
     def __init__(self, nome, vida, nivel, tipo):
         super().__init__(nome, vida, nivel)
@@ -41,11 +61,46 @@ class Inimigo(Personagem):
     
     def exibir_detalhes(self):
          return f'{super().exibir_detalhes()}\nTipo: {self.get_tipo()}\n'
-    
 
     
+class Jogo:
+    """" Classe orquestradora do jogo """
+
+    def __init__(self) -> None:
+         
+        self.heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
+        self.inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
+
+        
+    def iniciar_batalha(self):
+        """"Fazer a gestao da batalha"""
+        print("Iniciando a batalha")
+        while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
+            print("\nDetalhes dos Personagens:")
+            print(self.heroi.exibir_detalhes())
+            print(self.inimigo.exibir_detalhes())
+
+            input("Pressione Enter para atacar...")
+            escolha = input("Escolha (1 - Ataque normal, 2 - Ataque Especial): ")
+            
+            if escolha == "1":
+                 self.heroi.atacar(self.inimigo)
+            
+            elif escolha =='2':
+                 self.heroi.ataque_especial(self.inimigo)
+                  
+            else:
+                 print("Escolha invalida. Escolha novamente.") 
+
+            if self.inimigo.get_vida() > 0:
+                 # inimigo ataca heroi
+                self.inimigo.atacar(self.heroi)
+
+        if self.heroi.get_vida()> 0:
+             print("\nParabens, voce venceu a batalha!")
     
-heroi = Heroi(nome="Herói", vida=100, nivel=5, habilidade="Super Força")
-print(heroi.exibir_detalhes())
-inimigo = Inimigo(nome="Morcego", vida=50, nivel=3, tipo="Voador")
-print(inimigo.exibir_detalhes())
+
+# Crias instancias do jogo e iniciar batalha
+jogo = Jogo()
+jogo.iniciar_batalha()
+ 
